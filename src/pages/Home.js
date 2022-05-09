db9ce500cd240216d1ac6459bbc28dfb93d8625e
Josh <adesanyajoshua@ymail.com>
2022-05-09 22:05:27 +0100

    update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,24 +1,22 @@
 import React, { useEffect, useState, useRef } from "react";
-import { BigNumber, ethers } from "ethers";
+import { BigNumber, ethers, providers } from "ethers";
 import abi from "../utils/payment.json";
 import Web3Modal from "web3modal";
-import { providers } from "ethers";
 import WalletConnectProvider from "@walletconnect/web3-provider";
 import axios from "axios";
+import Web3 from "web3";
 
 export default function Home() {
-  const contractAddress = "0x62e2c7165Df60B3D079cAADfA11B769ea9CcB28E";
-  //const contractAddress = "0xc56bf3625ad76283f42c4df67cfd10f670a2528c";
-  //const contractAddress = "0xC56Bf3625aD76283f42c4DF67CFd10f670a2528C";
+  const contractAddress = "0x0f9ae0b2A73F0812010B9f96EE0767fC5d84e5d3";
 
   const contractABI = abi.abi;
   const [loading, setLoading] = useState(false);
   const [address, setAddress] = useState("");
   const [error, setError] = useState("");
   const [message, setMessage] = useState("");
-  const [balance, setBalance] = useState();
-  const [contractBalance, setContractBalance] = useState();
-  const [price, setPrice] = useState();
+  const [balance, setBalance] = useState(0);
+  const [contractBalance, setContractBalance] = useState(0);
+  const [price, setPrice] = useState(0);
 
   const [loading1, setLoading1] = useState(false);
   const [error1, setError1] = useState("");
@@ -28,36 +26,34 @@ export default function Home() {
   const etherAmountRef = useRef();
   const walletRef = useRef();
 
+  const web3ModalRef = useRef();
+  let provider;
+  let web3Modal;
+
   const providerOptions = {
     walletconnect: {
       package: WalletConnectProvider, // required
       options: {
-        infuraId: "c42bd204d32641289769eb8a9a4c1607", // required
+        infuraId: "", // required
       },
+    },
+    binancechainwallet: {
+      package: true,
     },
   };
 
   const getProviderOrSigner = async (needSigner = false) => {
     // Connect to Metamask
     // Since we store `web3Modal` as a reference, we need to access the `current` value to get access to the underlying object
-    const { ethereum } = window;
-
-    if (!ethereum) {
-      alert("Get MetaMask!");
-      return;
-    }
 
     web3Modal = new Web3Modal({
-      network: "rinkeby", // optional
+      network: "bsc", // optional
       cacheProvider: true, // optional
       providerOptions, // required
     });
 
     provider = await web3Modal.connect();
     const web3Provider = new providers.Web3Provider(provider);
-
-    const accounts = await ethereum.request({ method: "eth_accounts" });
-    setAddress(accounts[0]);
 
     // If user is not connected to the Mainnet network, let them know and throw an error
     const { chainId } = await web3Provider.getNetwork();
@@ -66,7 +62,7 @@ export default function Home() {
     // polygon - 80001
     if (chainId !== 4) {
       window.alert("Change the network to Rinkeby");
-      throw new Error("Change network to rinkeby");
+      throw new Error("Change network to Rinkeby");
     }
 
     if (needSigner) {
@@ -253,60 +249,52 @@ export default function Home() {
     }
   };
 
-  const web3ModalRef = useRef();
-  let provider;
-  let web3Modal;
-
   const connectWallet = async () => {
     try {
-      const { ethereum } = window;
+      // When used for the first time, it prompts the user to connect their wallet
+      const prov = await getProviderOrSigner();
 
-      if (ethereum) {
-        // Get the provider from web3Modal, which in our case is MetaMask
-        // When used for the first time, it prompts the user to connect their wallet
-        await getProviderOrSigner();
-        const accounts = await ethereum.request({ method: "eth_accounts" });
-        setAddress(accounts[0]);
-        const provider2 = await getProviderOrSigner();
-        const bal = await provider2.getBalance(accounts[0]);
+      const web3 = new Web3(provider);
+      const accounts = await web3.eth.getAccounts();
 
+      setAddress(accounts[0]);
+
+      const bal = await prov.getBalance(accounts[0]);
+
+      setBalance(Number(BigNumber.from(bal)) / 10 ** 18);
+
+      // track when wallet is connected
+      localStorage.setItem("wall", "true");
+
+      // Subscribe to accounts change
+      provider.on("accountsChanged", async (accounts) => {
+        const provider = await getProviderOrSigner();
+        const bal = await provider.getBalance(accounts[0]);
         setBalance(Number(BigNumber.from(bal)) / 10 ** 18);
+      });
 
-        localStorage.setItem("wall", "true");
+      // Subscribe to chainId change
+      provider.on("chainChanged", async (chainId) => {
+        console.log("chain", chainId);
+        await getProviderOrSigner();
+      });
 
-        // Subscribe to accounts change
-        provider.on("accountsChanged", async (accounts) => {
-          const provider = await getProviderOrSigner();
-          const bal = await provider.getBalance(accounts[0]);
-          setBalance(Number(BigNumber.from(bal)) / 10 ** 18);
-        });
+      // Subscribe to provider connection
+      provider.on("connect", async (info) => {
+        console.log("inf", info);
+      });
 
-        // Subscribe to chainId change
-        provider.on("chainChanged", async (chainId) => {
-          console.log("chain", chainId);
-          await getProviderOrSigner();
-        });
-
-        // Subscribe to provider connection
-        provider.on("connect", async (info) => {
-          console.log("inf", info);
-        });
-
-        // Subscribe to provider disconnection
-        provider.on("disconnect", (error) => {
-          console.log("dis", error);
-          setAddress("");
-        });
-      } else {
-        alert("Please install Metamask");
-      }
+      // Subscribe to provider disconnection
+      provider.on("disconnect", (error) => {
+        console.log("dis", error);
+        setAddress("");
+      });
     } catch (err) {
       console.error(err);
     }
   };
 
   const disconnectWallet = async () => {
-    setAddress("");
     localStorage.removeItem("wall");
     web3Modal = new Web3Modal({
       network: "rinkeby", // optional
@@ -314,24 +302,24 @@ export default function Home() {
       providerOptions, // required
     });
     await web3Modal.clearCachedProvider();
+    setAddress("");
   };
 
   useEffect(() => {
-    const { ethereum } = window;
+    if (localStorage.getItem("wall")) {
+      // Assign the Web3Modal class to the reference object by setting it's `current` value
+      // The `current` value is persisted throughout as long as this page is open
+      web3ModalRef.current = new Web3Modal({
+        network: "rinkeby",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
 
-    if (ethereum) {
-      if (localStorage.getItem("wall")) {
-        // Assign the Web3Modal class to the reference object by setting it's `current` value
-        // The `current` value is persisted throughout as long as this page is open
-        web3ModalRef.current = new Web3Modal({
-          network: "rinkeby",
-          providerOptions: {},
-          disableInjectedProvider: false,
-        });
+      connectWallet();
 
-        connectWallet();
-      }
+      setBalance(0);
     }
+
     // if wallet is not connected, create a new instance of Web3Modal and connect the MetaMask wallet
   }, [address]);
 
@@ -372,7 +360,7 @@ export default function Home() {
 
       <div className="balance">
         <div>
-          Balance <div>{balance} ether</div>
+          Balance <div>{balance.toFixed(4)} ether</div>
           <div>${(balance * price).toFixed(2)}</div>
         </div>
 
@@ -399,7 +387,7 @@ export default function Home() {
               placeholder="Enter wallet address"
             />
           </div>
-          <div style={{ color: "white", padding: "5px" }}>
+          <div style={{ color: "blue", padding: "5px" }}>
             {loading === true ? "Transaction in progess......." : ""}
           </div>
           <div style={{ color: "red", padding: "5px" }}>
@@ -422,7 +410,7 @@ export default function Home() {
               placeholder="Enter amount in ether"
             />
           </div>
-          <div style={{ color: "white", padding: "5px" }}>
+          <div style={{ color: "blue", padding: "5px" }}>
             {loading1 === true ? "Transaction in progess......." : ""}
           </div>
           <div style={{ color: "red", padding: "5px" }}>
